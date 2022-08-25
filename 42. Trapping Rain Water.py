ee92bd7d8c0f28b35569d2eadde54f6e763ccc3f
Connor Ross <coross3@outlook.com>
2022-08-25 16:17:01 -0700

    Update 42. Trapping Rain Water.py
    changed my tallest index getter to include some new things I learned!

--- 42. Trapping Rain Water.py
+++ 42. Trapping Rain Water.py
@@ -3,15 +3,18 @@
 # Something I have been doing to help me with problems is sketch them out on a sheet of notebook paper and rip out little peices for variables. 
 # This way I can follow the interpreter and catch any bugs/exceptions that may arise.
 #
+# Update! Started using pbd
+#
 ###
 
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-
-        tallest = max(dict(enumerate(height)), key=dict(enumerate(height)).get)
-        volume = 0
         
+        # get index of largest value in height
+        tallest = max([x for x in enumerate(height)], key=lambda x:x[1])[0]
+        
+        # runtime: O(n)
         def iterate(prev: int, tallest: int, step: int) -> int:
             volume = 0
             for i in range(prev, tallest, step):
@@ -21,4 +24,7 @@ class Solution:
                     volume += height[prev] - height[i]
             return volume
                 
-        return iterate(0, tallest, 1) + iterate(len(height) - 1, tallest, -1)
+        return (
+            iterate(0, tallest, 1) 
+            + iterate(len(height)-1, tallest, -1)
+        )

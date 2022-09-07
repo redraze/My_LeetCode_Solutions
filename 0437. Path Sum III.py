20aae873520376ff06439b3bb4369fc5fc0ea5dd
Connor Ross <coross3@outlook.com>
2022-09-07 15:30:21 -0700

    Update 0437. Path Sum III.py
    added some comments explaining a few of the conditional operators within the recurse function

--- 0437. Path Sum III.py
+++ 0437. Path Sum III.py
@@ -16,11 +16,16 @@ class Solution:
         if Sum == targetSum:
             paths += 1
         if root.left != None:
-            if copy == True:
-                paths += self.recurse(root.left, targetSum, Sum, depth+1, True)
+            # continue original root sum,
+            # and create new copy sum starting from 0 on next node
             if copy == False:
                 paths += self.recurse(root.left, targetSum, Sum, depth+1, False)
                 paths += self.recurse(root.left, targetSum, 0, depth+1, True)
+            # if root copy, only continue copy sum. 
+            # do not create new copy sum starting from 0 on next node,
+            # as this will be done by original root sum
+            else:
+                paths += self.recurse(root.left, targetSum, Sum, depth+1, True)
         if root.right != None:
             if copy == True:
                 paths += self.recurse(root.right, targetSum, Sum, depth+1, True)

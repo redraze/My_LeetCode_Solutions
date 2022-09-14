6e45c5e8fe235f7867fd94cece5b9652e7628549
Connor Ross <coross3@outlook.com>
2022-09-14 13:54:18 -0700

    Update 0542. 01 Matrix.py
    added a few more comments to further explain the algorithm

--- 0542. 01 Matrix.py
+++ 0542. 01 Matrix.py
@@ -11,32 +11,36 @@ class Solution:
         # maintain queue of squares that need their surroudning squares checked
         q = deque()
         
-        # mark zeros
+        # First pass:
+        # memoize and queue quares that contain zeros
         for i,j in itertools.product(range(m), range(n)):
             if mat[i][j] == 0:
                 mem.add((i,j))
                 q.append((i,j))
-
-          while q: # is not empty
-            i,j = q.popleft()
-            # check up
-            if i > 0 and (i-1,j) not in mem: 
-                mat[i-1][j] = mat[i][j] + 1
-                mem.add((i-1,j))
-                q.append((i-1,j))
-            # check down
-            if i < m-1 and (i+1,j) not in mem:
-                mat[i+1][j] = mat[i][j] + 1
-                mem.add((i+1,j))
-                q.append((i+1,j))
-            # check left
-            if j > 0 and (i,j-1) not in mem:
-                mat[i][j-1] = mat[i][j] + 1
-                mem.add((i,j-1))
-                q.append((i,j-1))
-            # check right
-            if j < n-1 and (i,j+1) not in mem:
-                mat[i][j+1] = mat[i][j] + 1
-                mem.add((i,j+1))
-                q.append((i,j+1))
-        return mat
+            
+        # Second pass:
+		# use the previously memoized and queued squares that countain zero to 
+		# iteratively compute the distances for the remaining non-memoized squares
+		while q: # is not empty
+			i,j = q.popleft()
+			# check up
+			if i > 0 and (i-1,j) not in mem: 
+				mat[i-1][j] = mat[i][j] + 1
+				mem.add((i-1,j))
+				q.append((i-1,j))
+			# check down
+			if i < m-1 and (i+1,j) not in mem:
+				mat[i+1][j] = mat[i][j] + 1
+				mem.add((i+1,j))
+				q.append((i+1,j))
+			# check left
+			if j > 0 and (i,j-1) not in mem:
+				mat[i][j-1] = mat[i][j] + 1
+				mem.add((i,j-1))
+				q.append((i,j-1))
+			# check right
+			if j < n-1 and (i,j+1) not in mem:
+				mat[i][j+1] = mat[i][j] + 1
+				mem.add((i,j+1))
+				q.append((i,j+1))
+		return mat

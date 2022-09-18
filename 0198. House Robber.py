5a2fd238e3c8d9ebe50f5457797edaf05cc448c3
Connor Ross <coross3@outlook.com>
2022-09-18 09:45:17 -0700

    Update 0198. House Robber.py
    added solution 2, an iterative solution, and some descriptions about runtime and space complexity for each solution

--- 0198. House Robber.py
+++ 0198. House Robber.py
@@ -1,4 +1,33 @@
 class Solution:
+    # Solution 2:
+    # iteration.
+    # O(n) runtime and space complexity.
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+        ans = nums[:]
+        for i in range(2, len(nums)):
+            if i > 2:
+                ans[i] = max(
+                    nums[i] + ans[i-3],
+                    nums[i] + ans[i-2],
+                    ans[i-1]
+                )
+                continue
+            ans[i] = max(
+                nums[i] + ans[i-2], 
+                ans[i-1]
+            )
+        return ans[-1]
+        
+    '''
+    # Solution 1:
+    # Recursion with memoization.
+    # O(n) runtime and O(n) space complexity.
+    # Uses more space than solution 2 though, since each 
+    # item in 'nums' requires it's own call to 'f()'
     def rob(self, nums: List[int]) -> int:
         # define a separate function that doesn't take a list
         # as an argument (otherwise the result would not be 
@@ -14,3 +43,4 @@ class Solution:
                 nums[i+1] + f(i+3)
             )
         return f(0)
+    '''

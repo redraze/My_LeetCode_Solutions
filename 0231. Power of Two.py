888b3d4c67cd275b886fb7d6e10d40d3ba944a81
Connor Ross <coross3@outlook.com>
2022-09-18 23:15:04 -0700

    Update 0231. Power of Two.py
    added a second solution that utilizes the bitwise operator 'and'

--- 0231. Power of Two.py
+++ 0231. Power of Two.py
@@ -1,4 +1,25 @@
 class Solution:
+    # Solution 2:
+    # bit magic
+    # O(n) runtime and space complexity
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n == 0:
+            return False
+        # using the bitwise 'and' operator (denoted as '&' in python)
+        # on any power of two and its immediatelly smaller integer,
+        # ie: 8 & 7,
+        # will result in a zero. any non-power of two integer will not 
+        # result in a zero.
+        # for example:
+        # bin(8) = '0b1000' --> 1000
+        # bin(7) = '0b111'  --> 0100
+        # 8 & 7 = 0
+        # the and operator will return a one in each digit's place only if both 
+        # places from bin(7) and bin(8) are one's, otherwise that digit's place
+        # will be zero
+        return not bool(n&n-1)
+    
+    
     # Solution 1:
     # examining binary equivalents
     # O(n) runtime and space complexity

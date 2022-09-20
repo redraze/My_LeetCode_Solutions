fab1f9add45007b26b8ca98e7f148f801fdc286b
Connor Ross <coross3@outlook.com>
2022-09-20 15:07:48 -0700

    Update 0082. Remove Duplicates from Sorted List II.py
    added a second solution that uses iteration

--- 0082. Remove Duplicates from Sorted List II.py
+++ 0082. Remove Duplicates from Sorted List II.py
@@ -1,4 +1,27 @@
 class Solution:
+    # Solution 2:
+    # iteration
+    # O(n) runtime
+    # O(1) space complexity
+    def deleteDuplicates(self, head: Optional[ListNode], prev=None) -> Optional[ListNode]:
+        pointer = ListNode(0, head)
+        prev = pointer
+        while head:
+            if head.next and head.val == head.next.val:
+                while head.next and head.val == head.next.val:
+                    head = head.next
+                head = head.next
+                prev.next = head
+                continue
+            head = head.next
+            prev = prev.next
+        return pointer.next
+    
+    '''
+    # Solution 1:
+    # recursion
+    # O(n) runtime
+    # O(n) space complexity
     def deleteDuplicates(self, head: Optional[ListNode], prev=None) -> Optional[ListNode]:
         if not head:
             return None
@@ -11,3 +34,4 @@ class Solution:
             return self.deleteDuplicates(head.next, prev)
         head.next = self.deleteDuplicates(head.next, head.val)
         return head
+    '''

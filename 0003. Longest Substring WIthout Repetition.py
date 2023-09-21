6d88e0c6aed9e5c024be6e221a0aba96caa45fdb
Connor Ross <coross3@outlook.com>
2023-09-21 11:22:58 -0700

    Update 0003. Longest Substring WIthout Repetition.py

--- 0003. Longest Substring WIthout Repetition.py
+++ 0003. Longest Substring WIthout Repetition.py
@@ -1,26 +1,32 @@
-###
-#
-# today I learned that hashtables have an average O(1) lookup time complexity, which is cool! 
-#
-###
-
-
+# most recent attempt: sliding window -- O(n) time and O(1) space complexity
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        hashTable = {}
-        anchor = 0
-        index = 0
-        max = 0
-        while index < len(s):
-            if len(s[anchor:index]) > max:
-                max = len(s[anchor:index])
-            while s[index] in hashTable:
-                hashTable.pop(s[anchor])
-                anchor += 1
-            hashTable[s[index]] = index
-            index += 1
+        letterSet, j, ans = set(), 0, 0
+        for i, val in enumerate(s):
+            while val in letterSet:
+                letterSet.remove(s[j])
+                j += 1
+            letterSet.add(val)
+            ans = max(ans, i - j + 1)
+        return ans
 
-        if len(s[anchor:index]) > max:
-            max = len(s[anchor:index])
+# # attempt #1
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         hashTable = {}
+#         anchor = 0
+#         index = 0
+#         max = 0
+#         while index < len(s):
+#             if len(s[anchor:index]) > max:
+#                 max = len(s[anchor:index])
+#             while s[index] in hashTable:
+#                 hashTable.pop(s[anchor])
+#                 anchor += 1
+#             hashTable[s[index]] = index
+#             index += 1
 
-        return max
+#         if len(s[anchor:index]) > max:
+#             max = len(s[anchor:index])
+
+#         return max

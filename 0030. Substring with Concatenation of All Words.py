5b6d006b766c7cb0d65889066ba833f5dc25e05e
Connor Ross <coross3@outlook.com>
2023-09-22 09:06:12 -0700

    Update 0030. Substring with Concatenation of All Words.py
    optimize the algorithm by not ever having to re-build an 'unUsed' dictionary

--- 0030. Substring with Concatenation of All Words.py
+++ 0030. Substring with Concatenation of All Words.py
@@ -1,25 +1,13 @@
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        # solution #1: brute force
-        # O(kn) runtime, where n = len(s) and k = len(words[i])
-        def removeWord(s: str, d: dict) -> dict:
-            if d[s] == 1:
-                del d[s]
-            else:
-                d[s] -= 1
-            return d
-        def addWord(s: str, d: dict) -> dict:
-            if s in d:
-                d[s] += 1
-            else:
-                d[s] = 1
-            return d
-        def resetDict(d: dict) -> dict:
-            for word in words:
-                addWord(word, d)
-            return d
+        # solution #1 -- O(kn) runtime (~7000ms lmaoooo)
+        # where n = len(s) and k = len(words[i])
 
-        used, unUsed = {}, resetDict({})
+        baseDict = {}
+        for word in words:
+            baseDict[word] = baseDict.get(word, 0) + 1
+
+        used = {}
         L = len(words[0])
         i, j, k = 0, 0, L
         ans = []
@@ -27,22 +15,20 @@ class Solution:
         while i <= len(s) - (L * len(words)):
             word = s[j:k]
 
-            if word in unUsed:
-                removeWord(word, unUsed)
-                addWord(word, used)
+            if used.get(word, 0) < baseDict.get(word, 0):
+                used[word] = used.get(word, 0) + 1
                 j += L
                 k += L
-
-                # all words have been used!
-                if unUsed == {}:
+                
+                if used == baseDict:
                     ans.append(i)
-                    used, unUsed = {}, resetDict({})
+                    used = {}
                     i += 1
                     j = i
                     k = i + L
 
             else:
-                used, unUsed = {}, resetDict({})
+                used = {}
                 i += 1
                 j = i
                 k = i + L

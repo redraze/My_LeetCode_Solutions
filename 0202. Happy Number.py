8cf58ea59d908a1adc21902c9b2849334e97c2ba
Connor Ross <coross3@outlook.com>
2023-09-27 17:52:23 -0700

    Update 0202. Happy Number.py

--- 0202. Happy Number.py
+++ 0202. Happy Number.py
@@ -1,16 +1,28 @@
+# solution #2
 class Solution:
     def isHappy(self, n: int) -> bool:
         seen = set()
-        num = n
-        while True:
-            sum = 0
-            for val in str(num):
-                sum += pow(int(val), 2)
-            print(sum)
-            if sum == 1:
-                return True
-            if sum in seen:
+        while n != 1:
+            n = sum([pow(int(val), 2) for val in str(n)])
+            if n in seen:
                 return False
-            seen.add(sum)
-            num = sum
-        return
+            seen.add(n)
+        return True
+
+# # solution #1
+# class Solution:
+#     def isHappy(self, n: int) -> bool:
+#         seen = set()
+#         num = n
+#         while True:
+#             sum = 0
+#             for val in str(num):
+#                 sum += pow(int(val), 2)
+#             print(sum)
+#             if sum == 1:
+#                 return True
+#             if sum in seen:
+#                 return False
+#             seen.add(sum)
+#             num = sum
+#         return

2ee67c4429b2d622bdf6cb4677a7f082cedffad3
Connor Ross <coross3@outlook.com>
2023-09-28 10:55:41 -0700

    Create 0056. Merge Intervals.py

---
+++ 0056. Merge Intervals.py
@@ -0,0 +1,18 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        ans = []
+        for i, interval in enumerate(
+            sorted(
+                intervals,
+                key=lambda interval: interval[0]
+            )
+        ):
+            if i == 0:
+                ans.append(interval)
+                continue
+            if ans[-1][1] >= interval[0]:
+                if interval[1] > ans[-1][1]:
+                    ans[-1][1] = interval[1]
+            else:
+                ans.append(interval)
+        return ans

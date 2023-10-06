551a170126b089b746464175077a159f69fd35ab
Connor Ross <coross3@outlook.com>
2023-10-06 12:02:49 -0700

    Update 0019. Remove Nth Node From End of List.py

--- 0019. Remove Nth Node From End of List.py
+++ 0019. Remove Nth Node From End of List.py
@@ -5,15 +5,34 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        if head == None:
-            return 1
-        temp = self.removeNthFromEnd(head.next, n)
-        if isinstance(temp, int) == True:
-            if temp == n:
-                return head.next
-            return 1 + temp
-        # modify current list
-        head.next = temp
+        # solution #2 -- two pointers
+        fast = head
+        for i in range(n):
+            fast = fast.next
+
+        if not fast:
+            head = head.next
+            return head
+
+        slow = prev = head
+        while fast:
+            prev = slow
+            slow = slow.next
+            fast = fast.next
+
+        prev.next = slow.next
         return head
-        # create new list to preserve input
-        # return ListNode(head.val, temp)
+
+        # # solution #1 -- recursion
+        # if head == None:
+        #     return 1
+        # temp = self.removeNthFromEnd(head.next, n)
+        # if isinstance(temp, int) == True:
+        #     if temp == n:
+        #         return head.next
+        #     return 1 + temp
+        # # modify current list
+        # head.next = temp
+        # return head
+        # # create new list to preserve input
+        # # return ListNode(head.val, temp)

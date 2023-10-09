276ef47c3f1e8d457d64ea89a6f18eead743b071
Connor Ross <coross3@outlook.com>
2023-10-09 11:55:14 -0700

    Update 0112. Path Sum.py

--- 0112. Path Sum.py
+++ 0112. Path Sum.py
@@ -4,26 +4,41 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# Most recent solution
 class Solution:
-    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        # empty tree
-        if root == None:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int, sum=0) -> bool:
+        if not root:
             return False
-
-        # leaf reached
-        if (
-            (
-                root.left == root.right == None
-                and targetSum - root.val == 0
-            )
-        ):
+        sum += root.val
+        if sum == targetSum and not root.left and not root.right:
             return True
+        return (
+            self.hasPathSum(root.left, targetSum, sum) 
+            or self.hasPathSum(root.right, targetSum, sum)
+        )
 
-        # continue with depth first search
-        if (
-            self.hasPathSum(root.right, targetSum - root.val)
-            or
-            self.hasPathSum(root.left, targetSum - root.val)
-        ):
-            return True
-        return False
+# # Previous solution
+# class Solution:
+#     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+#         # empty tree
+#         if root == None:
+#             return False
+
+#         # leaf reached
+#         if (
+#             (
+#                 root.left == root.right == None
+#                 and targetSum - root.val == 0
+#             )
+#         ):
+#             return True
+
+#         # continue with depth first search
+#         if (
+#             self.hasPathSum(root.right, targetSum - root.val)
+#             or
+#             self.hasPathSum(root.left, targetSum - root.val)
+#         ):
+#             return True
+#         return False

a96a750d592c641864a41e7853cb4930b333d61b
Connor Ross <coross3@outlook.com>
2023-10-11 09:51:20 -0700

    Create 0103. Binary Tree Zigzag Level Order Traversal.py

---
+++ 0103. Binary Tree Zigzag Level Order Traversal.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        q = deque([
+            [root, 0]
+        ])
+        ans = []
+        while q:
+            [node, depth] = q.popleft()
+            if not node:
+                continue
+            q.append([node.left, depth + 1])
+            q.append([node.right, depth + 1])
+            try:
+                ans[depth].append(node.val)
+            except IndexError:
+                ans.append([node.val])
+        
+        switch = False
+        for i in range(len(ans)):
+            if switch:
+                ans[i].reverse()
+            switch = not switch
+        return ans

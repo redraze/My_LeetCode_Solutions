5b365899a2f2728b14a9debf492e714532c61258
Connor Ross <coross3@outlook.com>
2023-10-16 16:44:09 -0700

    Update 0399. Evaluate Division.py
    use a collections defaultdict(dict) instead of a python dict with if statements

--- 0399. Evaluate Division.py
+++ 0399. Evaluate Division.py
@@ -1,18 +1,12 @@
-# overall O(n^2) time and space complexity
+# solution 1: build a relational graph, then BFS with a stack
+# O(n^2) time and space complexity (I think?)
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        # build graph of variable relations
-        # O(n) time and O(n^2) space complexity
-        graph = {}
-        for val, (var1, var2) in zip(values, equations):
-            if var1 not in graph:
-                graph[var1] = {}
-            if var2 not in graph:
-                graph[var2] = {}
-            graph[var1][var2] = val
-            graph[var2][var1] = 1 / val
+        graph = defaultdict(dict)
+        for val, (x, y) in zip(values, equations):
+            graph[x][y] = val
+            graph[y][x] = 1 / val
 
-        # O(n^2) time and O(n) space complexity
         ans = []
         for q1, q2 in queries:
             found = False

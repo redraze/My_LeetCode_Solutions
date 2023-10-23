8e3d6e1cd5ccd2f13b7ee46d21e9bff56f9228fd
Connor Ross <coross3@outlook.com>
2023-10-23 13:49:43 -0700

    Update 0046. Permutations.py

--- 0046. Permutations.py
+++ 0046. Permutations.py
@@ -1,17 +1,29 @@
+# my most recent attempt
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        # returns a list of the input list 
-        # if the input list only contains one number
-        if len(nums) < 2:
+        if len(nums) == 1:
             return [nums]
-        List = []
-        # iterate through each item in the input list...
+        ans = []
         for i, num in enumerate(nums):
-            # and append that item with each possible permutation of the 
-            # remainder of the input list
-            for j in self.permute(nums[0:i] + nums[i+1::]):
-                # store each of those items + permute appends in another list
-                # to be either returned to the top level call
-                # or passed up the recursion chain
-                List.append([nums[i]] + j)
-        return List
+            for p in self.permute(nums[:i] + nums[i + 1:]):
+                ans.append([num] + p)
+        return ans
+
+# # my previous attempt
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         # returns a list of the input list 
+#         # if the input list only contains one number
+#         if len(nums) < 2:
+#             return [nums]
+#         List = []
+#         # iterate through each item in the input list...
+#         for i, num in enumerate(nums):
+#             # and append that item with each possible permutation of the 
+#             # remainder of the input list
+#             for j in self.permute(nums[0:i] + nums[i+1::]):
+#                 # store each of those items + permute appends in another list
+#                 # to be either returned to the top level call
+#                 # or passed up the recursion chain
+#                 List.append([nums[i]] + j)
+#         return List

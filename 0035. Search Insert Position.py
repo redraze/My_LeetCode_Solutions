f47543b94fc7222c390a343a6b7ffed542fe3246
Connor Ross <coross3@outlook.com>
2023-10-27 12:11:22 -0700

    Update 0035. Search Insert Position.py

--- 0035. Search Insert Position.py
+++ 0035. Search Insert Position.py
@@ -1,16 +1,31 @@
 class Solution:
-    def searchInsert(self, nums: List[int], target: int, left=0) -> int:
-        # end conditions
+    # most recent solution -- iteration
+    def searchInsert(self, nums: List[int], target: int) -> int:
         if target > nums[-1]:
             return len(nums)
-        if target == nums[-1]:
-            return len(nums) - 1
-        if target < nums[0] or target == nums[0]:
+        if target < nums[0]:
             return 0
+
+        L, R = 0, len(nums)
+        while L < R:
+            mid = (L + R) // 2
+            if target > nums[mid]:
+                L = mid + 1 
+            else:
+                R = mid
+        return L
+
+    # # previous solution -- recursion
+    # def searchInsert(self, nums: List[int], target: int, left=0) -> int:
+    #     if target > nums[-1]:
+    #         return len(nums)
+    #     if target == nums[-1]:
+    #         return len(nums) - 1
+    #     if target < nums[0] or target == nums[0]:
+    #         return 0
         
-        # recurse conditions
-        right = len(nums) - 1
-        index = left + int((right-left)/2)
-        if target > nums[index]:
-            return self.searchInsert(nums, target, index+1)
-        return self.searchInsert(nums[0:index], target, left)
+    #     right = len(nums) - 1
+    #     index = left + int((right-left)/2)
+    #     if target > nums[index]:
+    #         return self.searchInsert(nums, target, index+1)
+    #     return self.searchInsert(nums[0:index], target, left)

e7549d4d7a2d2c09fb830c865467410d6e259287
Connor Ross <coross3@outlook.com>
2023-10-27 12:44:24 -0700

    Update 0162. Find Peak Element.py
    make recursive solution more legible

--- 0162. Find Peak Element.py
+++ 0162. Find Peak Element.py
@@ -14,7 +14,7 @@ class Solution:
     # def findPeakElement(self, nums: List[int], index=0) -> int:
     #     if len(nums) == 1:
     #         return index
-    #     m = len(nums)//2
-    #     if nums[m] > nums[m-1]:
-    #         return index + self.findPeakElement(nums[m::], m)
-    #     return self.findPeakElement(nums[0:m], index)
+    #     mid = len(nums) // 2
+    #     if nums[mid] > nums[mid - 1]:
+    #         return index + self.findPeakElement(nums[mid:], mid)
+    #     return self.findPeakElement(nums[:mid], index)

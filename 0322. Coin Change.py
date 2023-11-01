c3cd27bc4a89d69935bb828ce90683a954828bd8
Connor Ross <coross3@outlook.com>
2023-11-01 16:45:47 -0700

    Create 0322. Coin Change.py

---
+++ 0322. Coin Change.py
@@ -0,0 +1,36 @@
+class Solution:
+    # first attempt -- recursion with a memory cache
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0:
+            return 0 
+
+        # minimize sort time by eliminating un-usable coins beforehand
+        coins = [coin for coin in coins if coin <= amount]
+        coins = sorted(coins)
+
+        if not coins or coins[0] > amount:
+            return -1
+
+        def dp(coins: List[int], amount: int) -> int:
+            if amount in cache:
+                return cache[amount]
+
+            # limit list comprehension sizes in future recursions
+            coins = coins.copy()
+            while coins and coins[-1] > amount:
+                coins.pop()
+            if not coins:
+                return float('inf')
+
+            cache[amount] = min([
+                1 + dp(coins, amount - coin) for coin in coins[::-1]
+            ])
+
+            return cache[amount]
+        
+        cache = {0: 0}
+        dp(coins, amount)
+        
+        if cache[amount] == float('inf'):
+            return -1
+        return cache[amount]

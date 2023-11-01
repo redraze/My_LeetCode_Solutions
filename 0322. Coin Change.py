d16149cf87933ebcdd13f329df8be06f364f0863
Connor Ross <coross3@outlook.com>
2023-11-01 16:57:57 -0700

    Update 0322. Coin Change.py

--- 0322. Coin Change.py
+++ 0322. Coin Change.py
@@ -1,36 +1,51 @@
 class Solution:
-    # first attempt -- recursion with a memory cache
+    # Solution 2: tabulation
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if amount == 0:
-            return 0 
+        tab = [0] + [float('inf')] * (amount)
 
-        # minimize sort time by eliminating un-usable coins beforehand
         coins = [coin for coin in coins if coin <= amount]
         coins = sorted(coins)
 
-        if not coins or coins[0] > amount:
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                tab[i] = min(tab[i], tab[i - coin] + 1)
+
+        if tab[-1] == float('inf'):
             return -1
+        return tab[-1]
 
-        def dp(coins: List[int], amount: int) -> int:
-            if amount in cache:
-                return cache[amount]
+    # # Solution 1: recursion with memory cache
+    # def coinChange(self, coins: List[int], amount: int) -> int:
+    #     if amount == 0:
+    #         return 0 
 
-            # limit list comprehension sizes in future recursions
-            coins = coins.copy()
-            while coins and coins[-1] > amount:
-                coins.pop()
-            if not coins:
-                return float('inf')
+    #     # minimize sort time by eliminating un-usable coins beforehand
+    #     coins = [coin for coin in coins if coin <= amount]
+    #     coins = sorted(coins)
 
-            cache[amount] = min([
-                1 + dp(coins, amount - coin) for coin in coins[::-1]
-            ])
+    #     if not coins or coins[0] > amount:
+    #         return -1
 
-            return cache[amount]
+    #     def dp(coins: List[int], amount: int) -> int:
+    #         if amount in cache:
+    #             return cache[amount]
+
+    #         # limit list comprehension sizes in future recursions
+    #         coins = coins.copy()
+    #         while coins and coins[-1] > amount:
+    #             coins.pop()
+    #         if not coins:
+    #             return float('inf')
+
+    #         cache[amount] = min([
+    #             1 + dp(coins, amount - coin) for coin in coins[::-1]
+    #         ])
+
+    #         return cache[amount]
         
-        cache = {0: 0}
-        dp(coins, amount)
+    #     cache = {0: 0}
+    #     dp(coins, amount)
         
-        if cache[amount] == float('inf'):
-            return -1
-        return cache[amount]
+    #     if cache[amount] == float('inf'):
+    #         return -1
+    #     return cache[amount]

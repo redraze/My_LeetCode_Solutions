b111a3c78368ba78f97758f18fc2aa32b2c2a1f1
Connor Ross <coross3@outlook.com>
2023-12-20 09:20:40 -0800

    Update 0661. Image Smoother.py
    update solution

--- 0661. Image Smoother.py
+++ 0661. Image Smoother.py
@@ -1,30 +1,16 @@
 class Solution:
+    # O(m * n) time and space complexity
     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
-        w, h = len(img[0]), len(img)
+        h, w = len(img), len(img[0])
 
-        def findAverage(r: int, c: int, sum: int, count = 1) -> int:
-            dirs = [
-                [1, 0],     # up
-                [-1, 0],    # down
-                [0, 1],     # right
-                [0, -1],    # left
-                [1, 1],     # up right
-                [1, -1],    # up left
-                [-1, 1],    # down right
-                [-1, -1]    # down, left
-            ]
-
-            for dr, dc in dirs:
-                if 0 <= (r + dr) < h and 0 <= (c + dc) < w:
-                    count += 1
-                    sum += img[r + dr][c + dc]
+        rng = range(-1, 2)
+        def findAverage(r: int, c: int, sum = 0, count = 0) -> int:
+            for dr in rng:
+                for dc in rng:
+                    if 0 <= (r + dr) < h and 0 <= (c + dc) < w:
+                        count += 1
+                        sum += img[r + dr][c + dc]
 
             return int(sum / count)
 
-        ret = []
-        for i, row in enumerate(img):
-            ret.append([])
-            for j, num in enumerate(row):
-                ret[-1].append(findAverage(i, j, num))
-
-        return ret
+        return [[findAverage(r, c) for c in range(w)] for r in range(h)]

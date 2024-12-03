0d1f530e5be42f56e35499fdb8ca85ebe5577990
Connor Ross <coross3@outlook.com>
2024-12-03 09:24:40 -0800

    Update 0226. Invert Binary Tree.py
    new solution

--- 0226. Invert Binary Tree.py
+++ 0226. Invert Binary Tree.py
@@ -5,10 +5,33 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    # # Recursive solution
+    # def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+    #     if not root:
+    #         return
+    #     root.left, root.right = root.right, root.left
+    #     self.invertTree(root.left)
+    #     self.invertTree(root.right)
+    #     return root
+
+    # Iterative solution w/ stack
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
-            return
-        root.left, root.right = root.right, root.left
-        self.invertTree(root.left)
-        self.invertTree(root.right)
+            return root
+
+        stack = [root]
+        while stack:
+            for i in range(len(stack)):
+                node = stack.pop()
+
+                if not (node.left or node.right):
+                    continue
+
+                node.left, node.right = node.right, node.left
+
+                if node.left:
+                    stack.append(node.left)
+                if node.right:
+                    stack.append(node.right)
+
         return root
